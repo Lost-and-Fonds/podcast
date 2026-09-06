362182dafde20365a9dcbb8b834f5bd23d7c2567
Hazel <hazelfidecaro@gmail.com>
2026-09-06 17:56:11 +1200

    Load podcast plugin dependencies

--- stashd-plugin/plugin.php
+++ stashd-plugin/plugin.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 require_once '/sdk/bootstrap.php';
-require_once __DIR__ . '/../src/PodcastBroadcast.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Stashd\PluginSdk\Runtime\PluginServer(new Podcast\PodcastBroadcast()))->run();
